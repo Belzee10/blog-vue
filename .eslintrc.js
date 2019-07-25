e0ba6f302bcbcdd28188fe81ea23bd7cee3ec88f
Ivan <belzee92@gmail.com>
2019-07-25 16:11:53 -0400

    setup EsLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,31 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/strongly-recommended", "@vue/airbnb"],
+  extends: ['plugin:vue/strongly-recommended', '@vue/airbnb'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/max-attributes-per-line": [
-      2,
-      {
-        multiline: {
-          allowFirstLine: true
-        }
-      }
-    ],
-    quotes: ["error", "double"],
-    "max-len": ["error", { code: 140, ignoreStrings: true, ignoreUrls: true }],
-    "import/no-unresolved": 0,
-    "linebreak-style": 0,
-    "comma-dangle": 0,
-    "import/prefer-default-export": 0,
-    "no-unused-expressions": ["error", { allowTernary: true }],
-    "no-underscore-dangle": 0,
-    "no-param-reassign": 0,
-    "object-curly-newline": ["error", { ObjectPattern: "never" }],
-    "vue/html-closing-bracket-newline": [
-      "error",
-      {
-        singleline: "never",
-        multiline: "never"
-      }
-    ]
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'vue/max-attributes-per-line': [
+    //   2,
+    //   {
+    //     multiline: {
+    //       allowFirstLine: true,
+    //     },
+    //   },
+    // ],
+    // 'max-len': ['error', { code: 140, ignoreStrings: true, ignoreUrls: true }],
+    // 'object-curly-newline': ['error', { ObjectPattern: 'never' }],
+    // 'vue/html-closing-bracket-newline': [
+    //   'error',
+    //   {
+    //     singleline: 'never',
+    //     multiline: 'always',
+    //   },
+    // ],
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: 'babel-eslint',
+  },
 };
