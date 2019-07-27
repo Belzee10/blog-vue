8c1e11a90468986d048affc8e61cd0715a0f3d7f
Ivan <belzee92@gmail.com>
2019-07-27 10:42:53 -0400

    create Header and Text cmps

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'BaseHeader',
+  props: {
+    type: {
+      type: String,
+      default: 'h1'
+    }
+  },
+  render(createElement) {
+    return createElement(this.type, this.$slots.default);
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/BaseText.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'BaseText',
+  props: {
+    tag: {
+      type: String,
+      default: 'span',
+      validator(value) {
+        return ['span', 'p', 'label'].indexOf(value) !== -1;
+      }
+    },
+    htmlFor: {
+      type: String,
+      default: null
+    },
+    id: {
+      type: String,
+      default: null
+    }
+  },
+  render(createElement) {
+    return createElement(this.tag, this.$slots.default);
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
