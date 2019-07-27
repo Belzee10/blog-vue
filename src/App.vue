7041821a38ab642fb5bbd6b4352579f43bebf3ff
Ivan <belzee92@gmail.com>
2019-07-26 23:45:43 -0400

    create Nabvar cmp

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <Navbar />
-      <!-- <router-link to="/">
-        Home
-      </router-link>|
-      <router-link to="/about">
-        About
-      </router-link> -->
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
