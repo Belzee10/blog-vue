956c0b8017e48324344657a44f1da733dc1935d6
Ivan <belzee92@gmail.com>
2019-07-27 11:26:32 -0400

    add more base cmps

---
+++ src/components/BaseCol.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col" :class="getColWidth"></div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCol',
+  props: {
+    width: {
+      type: String,
+      default: '',
+      validator(value) {
+        return (
+          ['', '10', '20', '25', '33', '40', '50', '60', '67', '75', '80', '90'].indexOf(value) !==
+          -1
+        );
+      }
+    }
+  },
+  computed: {
+    getColWidth() {
+      return {
+        [`col-${this.width}`]: this.width
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -4,7 +4,11 @@ export default {
   props: {
     type: {
       type: String,
-      default: 'h1'
+      default: 'h1',
+      // TODO maybe create a util func for validate this
+      validator(value) {
+        return ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].indexOf(value) !== -1;
+      }
     }
   },
   render(createElement) {
